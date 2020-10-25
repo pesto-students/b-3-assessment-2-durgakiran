bf2fb5ce27634502d1a79385625331991f6fd007
durgakiran <gatemefuture@gmail.com>
2020-10-25 15:03:20 +0530

    react: Solve Autocomplete problem

--- src/autocomplete/Autocomplete.js
+++ src/autocomplete/Autocomplete.js
@@ -1,16 +1,62 @@
+import { debounce } from 'lodash';
 import React from 'react';
 import './Autocomplete.css';
 import { getData } from './getData';
 // Don't change any declarations.
 
 class Autocomplete extends React.Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className={'control'}></div>
-      </div>
-    );
-  }
+
+
+    state = {
+        suggestion: null,
+        isLoading: false,
+    }
+
+
+
+
+    getSuggestions = async (query) => {
+        this.setState({ isLoading: true });
+        const results = await getData(query);
+        console.log(results);
+        this.setState({ suggestion: results, isLoading: false })
+    }
+
+    debounceApi( query , time = 500) {
+        const fn = debounce(this.getSuggestions, time);
+        fn(query)
+    }
+
+    render() {
+        return (
+        <div className="wrapper">
+            <div className={ this.state.isLoading ?  'control' + ' ' + 'is-loading' : 'control' } data-testid="control">
+            <input placeholder="search...." 
+                    onChange={({target: { value }}) => this.debounceApi(value)}
+                    className="input"
+                    type="text"  
+                    name="autoComplete" 
+                    data-testid="auto-complete"/>
+            </div>
+            {
+                (this.state.suggestion && !this.state.isLoading) &&
+                    <div className="list">
+                        {
+                            this.state.suggestion && this.state.suggestion.map((item, index) => {
+                                return <a  
+                                        onClick={() => props.onSelectItem(item)}
+                                        className="list-item" 
+                                        key={index}>
+                                            {item}
+                                        </a>
+                            })
+                        }
+                    </div>
+
+            }
+        </div>
+        );
+    }
 }
 
 export { Autocomplete };
--- src/autocomplete/Autocomplete.test.js
+++ src/autocomplete/Autocomplete.test.js
@@ -1,6 +1,37 @@
-import {} from './Autocomplete';
+import { Autocomplete } from './Autocomplete';
+import  React from 'react';
+import ReactDOM from 'react-dom';
 
-// You can delete this test.
-test('test for checking', () => {
-  expect(true);
-});
+import { cleanup, fireEvent, render } from '@testing-library/react';
+
+describe('autocomplete', () => {
+    afterEach(cleanup);
+
+    test('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Autocomplete />, div);
+    });
+
+    // test('should match snapshot', () => {
+    //     const { asFragment } = render(<Autocomplete />);
+    //     expect(asFragment(<Autocomplete />)).toMatchSnapshot();
+    // });
+
+    test('should render a div element with class name control', () => { 
+        const { getByTestId } = render(<Autocomplete />);
+        expect(getByTestId('control')).toHaveClass('control');
+    });
+
+    test('should render input element inside a div element with class control', () => { });
+
+    test('should call debounce function on every keypress in the input', () => {
+        const debounceApi = jest.fn(); 
+        const { getByTestId } = render(<Autocomplete debounceApi={debounceApi}/>);
+        
+        const inputBox = getByTestId('auto-complete');
+        fireEvent.change(inputBox, { target: { value: 'q' } });
+
+        expect(debounceApi).toHaveBeenCalledTimes(1);
+    });
+
+})
