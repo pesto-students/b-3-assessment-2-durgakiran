59c0ed66fc8248af0c180888cc59dccaf4c0e450
durgakiran <gatemefuture@gmail.com>
2020-10-25 18:32:54 +0530

    js: Solve iterwindow problem

--- src/iter-window/iterWindow.js
+++ src/iter-window/iterWindow.js
@@ -1,3 +1,57 @@
-function* iterWindow() {}
+/**
+ * 
+ * @param {Iterable} iter 
+ * @param {Number} n 
+ * @param {any} fillValue 
+ * @param {*} step 
+ */
+function* iterWindow(iter, n, fillValue = null, step = 1) {
+    
+    if(!iter || typeof iter[Symbol.iterator] !== 'function') {
+        throw new TypeError(`Expected iterable, instead got ${iter}`);
+    }
+
+    if(!n || !Number.isSafeInteger(n)) {
+        throw new TypeError(`Expected a safe integer, instead got ${n}`);
+    }
+
+    if( step < 1 || !Number.isSafeInteger(step)) {
+        throw new TypeError(`Expected a safe integer and greater than 0, instead got ${step}`);
+    }
+
+
+
+    // make iterable to Array
+    const arr = Array.from(iter);
+
+    let numberOfStepsToSkip = step;
+
+    const numberOfWindows = (arr.length - n) > 0 ? ( arr.length - n + step ) : 1;
+
+    for(let i = 0; i < numberOfWindows; i += step) {
+
+
+        const tmpArray = [...arr];
+
+        const window = tmpArray.splice(i, n);
+
+
+        if( window.length < n ) {
+
+            while(window.length < n ) {
+                window.push(typeof fillValue === 'function' ? fillValue() : fillValue);
+            }
+        }
+
+        numberOfStepsToSkip = step - 1;
+
+        yield window;
+
+    }
+
+    
+
+
+}
 
 export { iterWindow };
--- src/iter-window/iterWindow.test.js
+++ src/iter-window/iterWindow.test.js
@@ -1,6 +1,56 @@
 import { iterWindow } from './iterWindow';
 
-// You can delete this test.
-test('test for checking', () => {
-  expect(true);
-});
+describe('Iterwindow', () => {
+    test('iter is a iterable', () => {
+        expect(() => iterWindow(1, 3).next()).toThrow();
+        expect(() => iterWindow({}, 3).next()).toThrow();
+        expect(() => iterWindow([1, 2 , 3], 3).next()).not.toThrow();
+    })
+
+    test('second argument must be a number', () => {
+        expect(() => iterWindow([1], '3').next()).toThrow();
+        expect(() => iterWindow([1], 3).next()).not.toThrow();
+    })
+
+    test('step should be greater than zero', () => {
+        expect(() => iterWindow([1, 2 , 3], 3).next()).not.toThrow();
+        expect(() => iterWindow([1, 2 , 3], 3, NaN,-1).next()).toThrow();
+        expect(() => iterWindow([1, 2 , 3], 3, NaN, 0).next()).toThrow();
+    })
+
+    test('should yield an Array for every step', () => {
+        let gen = iterWindow(new Map([[1, 'a'], [2, 'b'], [3, 'c']]), 3);
+        const window = gen.next();
+        expect(window.value).toBeInstanceOf(Array);
+        expect(window.value).toStrictEqual([[1, 'a'], [2, 'b'], [3, 'c']])
+    });
+
+    test('Array from of iterWindow  should be an Array', () => {
+        let gen = iterWindow(new Map([[1, 'a'], [2, 'b'], [3, 'c']]), 3);
+        expect(Array.from(gen)).toBeInstanceOf(Array);
+    })
+
+    test('Array from of iterWindow  should have proper length', () => {
+        let gen = iterWindow(new Map([[1, 'a'], [2, 'b'], [3, 'c']]), 3);
+        expect(Array.from(gen).length).toBe(1);
+        gen = iterWindow(new Map([[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']]), 3);
+        expect(Array.from(gen).length).toBe(2);
+    })
+
+    test('when array is insufficient length it should use fill value to fill reamianing indexes', () => {
+        let gen = iterWindow(new Map([[1, 'a'], [2, 'b']]), 3);
+        expect(gen.next().value).toStrictEqual([[1, 'a'], [2, 'b'], null]);
+        gen = iterWindow(new Map([[1, 'a'], [2, 'b']]), 3, NaN);
+        expect(gen.next().value).toStrictEqual([[1, 'a'], [2, 'b'], NaN]);
+    })
+
+    test('when step is provided it should skip elements in between', () => {
+        let gen = iterWindow(new Map([[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']]), 3, NaN, 2);
+        expect(Array.from(gen).length).toBe(2);
+        gen = iterWindow(new Map([[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']]), 3, NaN, 2);
+        expect(gen.next().value).toStrictEqual([[1, 'a'], [2, 'b'], [3, 'c']]);
+        const nextvalue = gen.next();
+        expect(nextvalue.done).toBe(false);
+        expect(nextvalue.value).toStrictEqual([[3, 'c'], [4, 'd'], NaN]);
+    })
+})
